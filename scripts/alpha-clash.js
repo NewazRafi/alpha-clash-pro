f871d83f5e7815c15b296bd7f4cd79e7e6053bc1
NewazRafi <mdnsrafi@gmail.com>
2024-02-21 07:34:20 +0600

    play now button functionality added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,12 @@
+// function play(){
+//     // hide the home screen
+//     const homeScreen = document.getElementById('home');
+//     homeScreen.classList.add('hidden')
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
 function play(){
-    console.log("Play clicked")
+    hideElementById('home');
+    showElementById('play-ground');
 }
